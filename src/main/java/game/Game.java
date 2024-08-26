a9ebb12b19fd9a526db952644f6637ec2ef9cca0
Thiago Barbosa <curiosidades381@gmail.com>
2024-08-26 03:29:19 -0300

    FPS | UPS
    *separação da lógica de update para a de renderização por segundos

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -7,7 +7,9 @@ public class Game implements Runnable {
 
     private GameWindow gameWindow;
     private GamePanel gamePanel;
-    private final int FPS_SET = 60;
+    private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
+
     private Thread gameThread;
 
     public Game() {
@@ -15,39 +17,51 @@ public class Game implements Runnable {
         this.gameWindow = new GameWindow(gamePanel);
 
         this.gamePanel.requestFocus();
+
         startGameLoop();
     }
 
     private void startGameLoop() {
         gameThread = new Thread(this);
-        gameThread.start(); // Use start() em vez de run()
+        gameThread.start();
     }
 
     @Override
     public void run() {
         long lastFrameTime = System.nanoTime();
+        long lastUpdateTime = System.nanoTime();
         long now;
         double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
 
         int frames = 0;
+        int updates = 0;
         long lastCheck = System.currentTimeMillis();
 
         while (true) {
             now = System.nanoTime();
-            double deltaTime = (now - lastFrameTime) / 1000000000.0; // Convertendo para segundos
 
+            // FPS Logic
             if (now - lastFrameTime >= timePerFrame) {
-                gamePanel.updateGame((float) deltaTime); // Passa deltaTime real para o GamePanel
                 gamePanel.repaint();
                 lastFrameTime = now;
                 frames++;
             }
 
-            // FPS display
+            // UPS Logic
+            if (now - lastUpdateTime >= timePerUpdate) {
+                double deltaTime = (now - lastUpdateTime) / 1000000000.0; // Convertendo para segundos
+                gamePanel.updateGame((float) deltaTime); // Passa deltaTime real para o GamePanel
+                lastUpdateTime = now;
+                updates++;
+            }
+
+            // FPS and UPS Display
             if (System.currentTimeMillis() - lastCheck >= 1000) {
                 lastCheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames);
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
                 frames = 0;
+                updates = 0;
             }
         }
     }
